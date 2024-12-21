eaff023a9311908aae6dc436cd9580847daba2e1
Ossi Perä <ossi.pera@buutti.com>
2024-12-21 16:22:03 +0200

    use info state

--- src/info_state.rs
+++ src/info_state.rs
@@ -1,3 +1,30 @@
+#[derive(Debug, Clone)]
 pub struct InfoState {
     info_state: Vec<String>,
+}
+
+impl InfoState {
+    pub fn new() -> Self {
+        InfoState {
+            info_state: Vec::new(),
+        }
+    }
+
+    pub fn push(&mut self, action: String) {
+        self.info_state.push(action);
+    }
+
+    pub fn to_string(&self) -> String {
+        self.info_state.join("")
+    }
+
+    pub fn clone(&self) -> Self {
+        InfoState {
+            info_state: self.info_state.clone(),
+        }
+    }
+
+    pub fn into_vec(self) -> Vec<String> {
+        self.info_state
+    }
 }
--- src/kuhn.rs
+++ src/kuhn.rs
@@ -1,11 +1,12 @@
 use rand::seq::SliceRandom;
 use rand::rngs::StdRng;
 use crate::player::Player;
+use crate::info_state::InfoState;
 
 #[derive(Clone, Debug)]
 pub struct Kuhn {
     cards: Vec<usize>,
-    history: Vec<String>,
+    info_state: InfoState,
     player: Player,
 }
 
@@ -16,7 +17,7 @@ impl Kuhn {
         cards.shuffle(&mut rng);
         Kuhn {
             cards,
-            history: Vec::new(),
+            info_state: InfoState::new(),
             player: Player::IP,
         }
     }
@@ -26,45 +27,45 @@ impl Kuhn {
     }
 
     pub fn is_terminal(&self) -> bool {
-        let history_str = self.history.join("");
-        history_str == "pbp" ||
-        history_str == "pbb" ||
-        history_str == "pp" ||
-        history_str == "bp" ||
-        history_str == "bb"
+        let info_state_str = self.info_state.to_string();
+        info_state_str == "pbp" ||
+        info_state_str == "pbb" ||
+        info_state_str == "pp" ||
+        info_state_str == "bp" ||
+        info_state_str == "bb"
     }
 
     pub fn get_payoff(&self, player: Player) -> f64 {
         let opponent = if player == Player::IP { Player::OOP } else { Player::IP };
-        let history_str = self.history.join("");
-        if history_str == "pp" {
+        let info_state_str = self.info_state.to_string();
+        if info_state_str == "pp" {
             if self.cards[player as usize] > self.cards[opponent as usize] {
                 1.0
             } else {
                 -1.0
             }
-        } else if history_str == "bp" || history_str == "pbp" {
+        } else if info_state_str == "bp" || info_state_str == "pbp" {
             1.0
-        } else if history_str == "bb" || history_str == "pbb"{
+        } else if info_state_str == "bb" || info_state_str == "pbb"{
             if self.cards[player as usize] > self.cards[opponent as usize] {
                 2.0
             } else {
                 -2.0
             }
         } else {
-            panic!("Invalid game state: history = {:?}, cards = {:?}", self.history, self.cards)
+            panic!("Invalid game state: history = {:?}, cards = {:?}", self.info_state, self.cards)
         }
     }
 
     pub fn next_state(&self, action: &str) -> Kuhn {
         let mut next_state = self.clone();
-        next_state.history.push(action.to_string());
+        next_state.info_state.push(action.to_string());
         next_state.player = if self.player == Player::IP { Player::OOP } else { Player::IP };
         next_state
     }
 
     pub fn get_history(&self) -> Vec<String> {
-        self.history.clone()
+        self.info_state.clone().into_vec()
     }
 
     pub fn get_cards(&self) -> Vec<usize> {
@@ -75,7 +76,7 @@ impl Kuhn {
         self.cards[self.player as usize]
     }
 
-    pub fn get_history_str(&self) -> Vec<String> {
-        self.history.clone()
+    pub fn get_info_state_str(&self) -> Vec<String> {
+        self.info_state.clone().into_vec()
     }
 }
