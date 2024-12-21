86c1b7a1583c5dbef178814d33c86bd017784837
Ossi Perä <ossi.pera@buutti.com>
2024-12-21 19:30:01 +0200

    add kuhn tests

--- src/kuhn.rs
+++ src/kuhn.rs
@@ -2,21 +2,23 @@ use rand::seq::SliceRandom;
 use rand::rngs::StdRng;
 use crate::action::Action;
 use crate::info_state::InfoState;
+use crate::node::Node;
 
 #[derive(Clone, Debug)]
 pub struct Kuhn {
-    cards: Vec<usize>
 }
 
 impl Kuhn {
     pub fn new() -> Self {
-        Kuhn {
-            cards: vec![0, 1, 2],
-        }
+        Kuhn {}
+    }
+
+    pub fn deck(&self) -> Vec<usize> {
+        vec![0, 1, 2]
     }
 
     pub fn shuffled_cards(&self, rng: &mut StdRng) -> Vec<usize> {
-        let mut cards = self.cards.clone();
+        let mut cards = self.deck();
         cards.shuffle(rng);
         cards
     }
@@ -31,12 +33,11 @@ impl Kuhn {
         }
     }
 
-    pub fn get_payoff(&self, info_state: &InfoState) -> f64 {
-        let player = info_state.player();
-        let opponent = player.opponent();
+    pub fn get_payoff(&self, node: &Node) -> f64 {
+        let info_state = &node.info_state;
         let actions: Vec<Action> = info_state.history().to_vec();
         if actions == vec![Action::Check, Action::Check] {
-            if self.cards[player.as_usize()] > self.cards[opponent.as_usize()] {
+            if node.player_cards() > node.opponent_cards() {
                 1.0
             } else {
                 -1.0
@@ -44,13 +45,54 @@ impl Kuhn {
         } else if actions == vec![Action::Bet(50), Action::Fold] || actions == vec![Action::Check, Action::Bet(50), Action::Fold] {
             1.0
         } else if actions == vec![Action::Bet(50), Action::Call] || actions == vec![Action::Check, Action::Bet(50), Action::Call] {
-            if self.cards[player.as_usize()] > self.cards[opponent.as_usize()] {
+            if node.player_cards() > node.opponent_cards() {
                 2.0
             } else {
                 -2.0
             }
         } else {
-            panic!("Invalid game state: history = {:?}, cards = {:?}", actions, self.cards);
+            panic!("Invalid game state: history {:?}, player cards {:?}, opponent cards {:?}",
+                actions, node.player_cards(), node.opponent_cards());
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_kuhn_deck() {
+        let kuhn = Kuhn::new();
+        assert_eq!(kuhn.deck(), vec![0, 1, 2]);
+    }
+
+    #[test]
+    fn test_legal_actions_at_root() {
+        let kuhn = Kuhn::new();
+        let info_state = InfoState::new(0);
+        let actions = kuhn.get_legal_actions(&info_state);
+        assert_eq!(actions, vec![Action::Check, Action::Bet(50)]);
+    }
+
+    #[test]
+    fn test_legal_actions_after_check() {
+        let kuhn = Kuhn::new();
+        let info_state = InfoState::new(0).next_info_state(Action::Check, 0);
+        let actions = kuhn.get_legal_actions(&info_state);
+        assert_eq!(actions, vec![Action::Check, Action::Bet(50)]);
+    }
+
+    #[test]
+    fn test_legal_actions_after_bet() {
+        let kuhn = Kuhn::new();
+        let info_state = InfoState::new(0).next_info_state(Action::Bet(50), 0);
+        let actions = kuhn.get_legal_actions(&info_state);
+        assert_eq!(actions, vec![Action::Call, Action::Fold]);
+    }
+
+    #[test]
+    fn test_payoff_after_check_check() {
+        
     }
 }
