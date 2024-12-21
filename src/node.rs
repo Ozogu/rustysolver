6cb500c87bad3394227843150f5961e1ee8c1053
Ossi Perä <ossi.pera@buutti.com>
2024-12-21 18:14:14 +0200

    implement node

--- src/node.rs
+++ src/node.rs
@@ -1,11 +1,38 @@
 use std::collections::HashMap;
 use crate::player::Player;
-use crate::action::Action;
 use crate::info_state::InfoState;
+use crate::action::Action;
 
-struct Node {
-    player: Player,
-    actions: Vec<Action>,
-    reach_prob: HashMap<Player, f64>,
-    info_state: InfoState,
+#[derive(Clone, Debug)]
+pub struct Node {
+    pub info_state: InfoState,
+    pub reach_prob: HashMap<Player, f64>,
+}
+
+impl Node {
+    pub fn new(cards: Vec<usize>) -> Self {
+        Node {
+            info_state: InfoState::new(cards),
+            reach_prob: HashMap::from([(Player::IP, 1.0), (Player::OOP, 1.0)]),
+        }
+    }
+
+    pub fn is_terminal(&self) -> bool {
+        self.info_state.history().is_terminal()
+    }
+    
+    pub fn player(&self) -> Player {
+        self.info_state.player()
+    }
+
+    pub fn reach_prob(&self) -> f64 {
+        self.reach_prob[&self.player()]
+    }
+
+    pub fn next_node(&self, action: Action, action_prob: f64) -> Node {
+        let mut next_node: Node = self.clone();
+        next_node.info_state = next_node.info_state.next_info_state(action);
+        next_node.reach_prob.insert(self.player(), self.reach_prob() * action_prob);
+        next_node
+    }
 }
