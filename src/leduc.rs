fcdd6b196232c4e605d705e8fe6b5e32b203e79c
Ossi Perä <ossi.pera@buutti.com>
2024-12-23 11:36:14 +0200

    add leduc skeleton

---
+++ src/leduc.rs
@@ -0,0 +1,47 @@
+use crate::deck::Deck;
+use crate::game::Game;
+use crate::suit::Suit;
+use crate::card::Card;
+use crate::pot::Pot;
+use crate::action::Action;
+use crate::info_state::InfoState;
+use crate::node::Node;
+
+pub struct Leduc {}
+
+impl Leduc {
+    pub fn new() -> Self {
+        Leduc {}
+    }
+}
+
+impl Game for Leduc {
+    fn initial_pot(&self) -> Pot {
+        Pot::new(1.0, 1.0)
+    }
+
+    fn deck(&self) -> crate::deck::Deck {
+        Deck::new_from_cards(
+            vec![
+                Card::new(1, Suit::Diamonds),
+                Card::new(1, Suit::Clubs),
+                Card::new(2, Suit::Diamonds),
+                Card::new(2, Suit::Clubs),
+                Card::new(3, Suit::Diamonds),
+                Card::new(3, Suit::Clubs),
+            ]
+        )   
+    }
+
+    fn num_streets(&self) -> u8 {
+        2
+    }
+
+    fn get_legal_actions(&self, info_state: &InfoState) -> Vec<Action> {
+        vec![]
+    }
+
+    fn player_wins(&self, node: &Node) -> Option<bool> {
+        None
+    }
+}
--- tests/itest.rs
+++ tests/itest.rs
@@ -1,11 +1,24 @@
 use rustysolver::cfr::CFR;
 use rustysolver::kuhn::Kuhn;
+use rustysolver::leduc::Leduc;
 
 #[test]
 fn test_kuhn_poker_ev() {
     let mut cfr = CFR::new(Kuhn::new());
     let ev = cfr.train(12000);
     let ideal_ev = -1.0/18.0;
+    let ev_diff = (ev - ideal_ev).abs();
+    
+    debug_assert!(ev_diff < 0.0011, 
+        "EV: {:.4}, Ideal: {:.4}, Diff: {:.4}",
+        ev, ideal_ev, ev_diff);
+}
+
+#[test]
+fn test_leduc_poker_ev() {
+    let mut cfr = CFR::new(Leduc::new());
+    let ev = cfr.train(12000);
+    let ideal_ev = 0.0;
     let ev_diff = (ev - ideal_ev).abs();
     
     debug_assert!(ev_diff < 0.0011, 
