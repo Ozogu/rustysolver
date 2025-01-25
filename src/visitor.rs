17419f5f9c3e265abf80430a55a2fb5184a95985
Ossi Perä <ossi.pera@buutti.com>
2025-01-25 15:56:25 +0200

    update visitor interface

--- src/visitor.rs
+++ src/visitor.rs
@@ -1,7 +1,11 @@
-use crate::node::Node;
+use crate::node::{self, Node};
 
 pub trait Visitor {
     fn visit_terminal_node(&mut self, node: &Node);
     fn visit_street_completing_node(&mut self, node: &Node);
     fn visit_action_node(&mut self, node: &Node);
+
+    fn get_action_probs(&self, node: &Node) -> Vec<f64> {
+        vec![1.0; node.actions.len()]
+    }
 }
