f2959d855d1c1a4e148ee42eaab1b1e40e38cb57
Ossi Perä <ossi.pera@buutti.com>
2025-01-25 15:34:23 +0200

    implement 2 ways to iterate tree

--- src/game_tree.rs
+++ src/game_tree.rs
@@ -2,9 +2,9 @@ use std::collections::HashMap;
 use crate::info_state::InfoState;
 use crate::game::Game;
 use crate::node::Node;
-use crate::visitable::Visitable;
 use crate::visitor::Visitor;
-use std::boxed::Box;
+use rand::rngs::StdRng;
+use rand::SeedableRng;
 
 pub struct GameTree<G: Game> {
     regrets: HashMap<InfoState, Vec<f64>>,
@@ -23,10 +23,7 @@ impl<G: Game> GameTree<G> {
 
     pub fn build(&mut self) {
         let mut builder_visitor = BuilderVisitor::new();
-        self.game.generate_deals().iter().for_each(|deal| {
-            let node = Node::new(&self.game, deal.clone());
-            self.iterate_tree(node, &mut builder_visitor);
-        });
+        self.walk_tree(&mut builder_visitor);
 
         self.regrets = builder_visitor.regrets;
         self.strategy_sum = builder_visitor.strategy_sum;
@@ -38,7 +35,21 @@ impl<G: Game> GameTree<G> {
         }
     }
 
-    fn iterate_tree<V: Visitor>(&mut self, node: Node, visitor: &mut V) {
+    pub fn walk_tree<V: Visitor>(&self, visitor: &mut V) {
+        let rng = &mut StdRng::seed_from_u64(0);
+        self.game.generate_deals().iter().for_each(|deal| {
+            let node = Node::new(&self.game, deal.clone());
+            self.iterate_tree(node, rng, &WalkMethod::Full, visitor);
+        });
+    }
+
+    pub fn monte_carlo_iterate<V: Visitor>(&self, rng: &mut StdRng, visitor: &mut V) {
+        let deal = self.game.deal(rng);
+        let node = Node::new(&self.game, deal);
+        self.iterate_tree(node, rng, &WalkMethod::MonteCarlo, visitor);
+    }
+
+    fn iterate_tree<V: Visitor>(&self, node: Node, rng: &mut StdRng, method: &WalkMethod, visitor: &mut V) {
         if node.is_terminal(&self.game) {
             visitor.visit_terminal_node(&node);
 
@@ -46,20 +57,33 @@ impl<G: Game> GameTree<G> {
         } else if node.is_street_completing_action() {
             visitor.visit_street_completing_node(&node);
 
-            for card in node.deck.iter() {
-                let next_street = node.history.street().next_street(card.clone());
-                let next_node = node.next_street_node(&self.game, next_street);
-                self.iterate_tree(next_node, visitor);
+            match method {
+                WalkMethod::MonteCarlo => {
+                    let card = node.deck.get(0).unwrap();
+                    let next_street = node.history.street().next_street(card.clone());
+                    let mut next_node = node.next_street_node(&self.game, next_street);
+                    next_node.deck.draw();
+
+                    self.iterate_tree(next_node, rng, method, visitor);
+                }
+                WalkMethod::Full => {
+                    for card in node.deck.iter() {
+                        let next_street = node.history.street().next_street(card.clone());
+                        let next_node = node.next_street_node(&self.game, next_street);
+                        self.iterate_tree(next_node, rng, method, visitor);
+                    }
+                }
             }
 
             return;
         } else {
             visitor.visit_action_node(&node);
 
+            // TODO: implement walk methods
             for action in node.actions.iter() {
                 let new_node = node.next_action_node(&self.game, action.clone(), 1.0);
 
-                self.iterate_tree(new_node, visitor);
+                self.iterate_tree(new_node, rng, method, visitor);
             }
         }
     }
@@ -68,6 +92,11 @@ impl<G: Game> GameTree<G> {
 struct BuilderVisitor {
     pub regrets: HashMap<InfoState, Vec<f64>>,
     pub strategy_sum: HashMap<InfoState, Vec<f64>>,
+}
+
+enum WalkMethod {
+    MonteCarlo,
+    Full,
 }
 
 impl BuilderVisitor {
